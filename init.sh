112ddc0debd5d688c7cafc431c683302a8e8e4cf
Takahiro Nishimura <tkhr.nishimura@gmail.com>
2016-07-01 19:47:39 +0900

    add init.sh

---
+++ init.sh
@@ -0,0 +1,5 @@
+cp ./.zshrc ../
+cp ./.vimrc ../
+ls -s ./.vim ../.vim
+git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
