fcd8ebcc9087c0ac164743c46df53184a33a0050
Takahiro Nishimura <tkhr.nishimura@gmail.com>
2016-07-02 15:25:24 +0900

    add atom package

--- brew.sh
+++ brew.sh
@@ -41,6 +41,7 @@ brew cask install virtualbox
 brew cask install vagrant
 brew cask install intellij-idea
 brew cask install alfred
+brew cask install atom
 
 mas install 539883307
 mas install 414855915
@@ -68,27 +69,69 @@ mas install 714196447
 mas install 449589707
 mas install 402398561
 
-
+# Desktopにファイルを表示しない
 defaults write com.apple.finder CreateDesktop -bool false
 defaults write com.apple.finder NewWindowTarget -string "PfLo"
+
+# Finderで新規ウィンドウを開くときはホームに設定
 defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}"
+
+# 拡張子変更で怒られない
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+defaultps write com.apple.finder FXDefaultSearchScope -string "SCcf"
 defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
 defaults write com.apple.finder ShowRecentTags -bool false
 defaults write com.apple.finder SidebarDevicesSectionDisclosedState -bool true
 defaults write com.apple.finder SidebarPlacesSectionDisclosedState -bool true
+
+
 defaults write com.apple.finderSidebarSharedSectionDisclosedState -bool true
+
+## 拡張子を全部表示
 defaults write -g AppleShowAllExtensions -bool true
+
+## ネットワーク上のディスクに.DS_Storeを作らない
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+## キーリピートを爆速にする
 defaults write -g KeyRepeat -int 2
 defaults write -g InitialKeyRepeat -int 15
+
+# Quicklookに表示されるテキストをコピペ可能にする
 defaults write com.apple.finder QLEnableTextSelection -bool true
+
+
+# スクショの設定
+## 保存をjpgにする
 defaults write com.apple.screencapture type jpg
+
+## スクショに影をつけない
 defaults write com.apple.screencapture "disable-shadow" -bool yes
+
+## スクショの名前をssからはじめる
 defaults write com.apple.screencapture name ss
+
+## 保存先を$HOME/ssにする
 defaults write com.apple.screencapture location ~/ss/
 
 killall Dock
 killall SystemUIServer
 killall Finder
+
+
+## Atomの設定
+apm install atom-material-ui
+apm install markdown-preview
+apm install autocomplete-paths
+apm install color-picker
+apm install highlight-selected
+apm install minimap
+apm install minimap-git-diff
+apm install pigments
+apm install git-time-machine
+apm install autocomplete-sass
+apm install merge-conflicts
+apm install vim-mode-plus
+apm install file-icons
+apm install terminal-plus
+
